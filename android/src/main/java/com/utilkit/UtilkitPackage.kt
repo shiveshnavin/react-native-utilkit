0e72ebe1277db8ba927e24af438783cd81695739
Shivesh Navin <you@example.com>
2024-08-26 14:48:50 +0530

    chore: initial commit

---
+++ android/src/main/java/com/utilkit/UtilkitPackage.kt
@@ -0,0 +1,17 @@
+package com.utilkit
+
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ViewManager
+
+
+class UtilkitPackage : ReactPackage {
+  override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+    return listOf(UtilkitModule(reactContext))
+  }
+
+  override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
+    return emptyList()
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  UtilkitExample
+//
+
+import Foundation
